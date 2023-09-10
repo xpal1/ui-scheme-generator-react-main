420316d586af39a86f01f9e1abd9a01b1f8044f9
xpal1 <palikorabsky@gmail.com>
2023-09-10 23:22:34 +0200

    fixnuty bug s neviditelnymi itemami v CSS, dropdown s info o polozke namiesto mazania

--- client/src/components/ItemsList/ItemsList.js
+++ client/src/components/ItemsList/ItemsList.js
@@ -1,36 +1,49 @@
-import React from "react";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import React, { useState } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import InfoIcon from "@mui/icons-material/Info";
 import "./itemsList.css";
 
-function ItemsList({ schema, newState, itemKey, index, setState }) {
+function ItemsList({ schema }) {
+  const [showInfo, setShowInfo] = useState(false);
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-      }}
-    >
-      {schema.iTitle}
-      <input
-        type={schema.iType}
-        placeholder={schema.iField}
-        required={schema.iIsRequired}
-        max={schema.iMaxValue}
-        maxLength={schema.iMaxLength}
-        tabIndex={schema.iTabIndex}
-        className="items-list-input"
-      />
-      <button
-        type="button"
-        className="delete-btn"
-        onClick={() => {
-          newState[itemKey] = newState[itemKey] || [];
-          newState[itemKey].splice(index, 1);
-          setState(newState.filter((group) => group.length));
-        }}
-      >
-        <DeleteOutlineIcon />
-      </button>
+    <div className="items-list-container">
+      <div className="items-list-header">
+        {schema.iTitle} &nbsp;{" "}
+        <InfoIcon onClick={toggleInfo} sx={{ color: "#0e94e2" }} />
+        <button
+          type="button"
+          className="info-dropdown-btn"
+          onClick={toggleInfo}
+        >
+          {showInfo ? (
+            <ExpandLessIcon sx={{ ml: "-1rem" }} />
+          ) : (
+            <ExpandMoreIcon sx={{ ml: "-1rem" }} />
+          )}
+        </button>
+      </div>
+      {showInfo && (
+        <div className="info-dropdown-content">
+          <p>Názov položky: {schema.iTitle}</p>
+          <p>Typ položky: {schema.iType}</p>
+          <p>Popis (pole) položky: {schema.iField}</p>
+          <p>Je povinná: {schema.iIsRequired ? "áno" : "nie"}</p>
+          <p>
+            Maximálna hodnota:{" "}
+            {schema.iMaxValue ? schema.iMaxValue : "nie je určená"}
+          </p>
+          <p>
+            Maximálna dĺžka:{" "}
+            {schema.iMaxLength ? schema.iMaxLength : "nie je určená"}
+          </p>
+          <p>Tab Index: {schema.iTabIndex}</p>
+        </div>
+      )}
     </div>
   );
 }
